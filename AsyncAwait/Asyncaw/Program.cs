564a629a15f79f75d8c34f3e7384c17c6a0a3b89
Aleksey Potapov <alexii.potapov@gmail.com>
2021-11-15 23:12:52 +0300

    initial

---
+++ AsyncAwait/Asyncaw/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Asyncaw
+{
+    class Program
+    {
+        static async Task<int> MMain() { 
+             Console.WriteLine("3");
+            return 2;
+        }
+
+        static void Main() => MMain().GetAwaiter().GetResult();
+    }
+}
